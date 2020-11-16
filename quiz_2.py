db1074f9a68f3560d1a22e3cfcd46d92de7f8ab8
John Doe <johndoe@example.com>
2020-11-16 00:15:42 -0600

    almost done

--- quiz_2.py
+++ quiz_2.py
@@ -1,44 +1,32 @@
 import pandas as pd 
 
-classes = pd.read_csv("animal_classes.csv", usecols=["Class_Number","Class_Type"])
+#x and y 
 train = pd.read_csv("animals_train.csv")
+df_train = pd.DataFrame(train.iloc[:, :16])
+df_target = pd.Series(train.iloc[:, 16])
+
+#testing the training module
 test = pd.read_csv("animals_test.csv")
+df_test = pd.DataFrame(test.iloc[:, 1:])
 
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
 
-df_class = pd.DataFrame(classes)
-df_train = pd.DataFrame(train)
-df_test = pd.DataFrame(test)
+knn.fit(X=df_train, y=df_target)
+predicted = knn.predict(df_test)
+
+df_output = pd.DataFrame()
+df_output["class_number"] = pd.Series(predicted)
 
 my_dict = {1: "Mammal", 2: "Bird", 3: "Reptile", 4: "Fish", 5: "Amphibian", 6: "Bug", 7: "Invertibrate"}
-
 types = []
 
-for index, key in df_train["class_number"].items():
+for index, key in df_output["class_number"].items():
     value = my_dict.get(key, "no num")
     types.append(value)
 
-df_train["class_type"] = types
-del df_train["class_number"]
-
-print(df_train)
-
-from sklearn.linear_model import LinearRegression
-
-lr = LinearRegression() 
-
-lr.fit(X=df_train, y=df_train["class_type"])
-
-test_data = df_test.loc[0:36, "hair":"catsize"]
-test_target = pd.Series(df_test["animal_name"])
-
-predicted = lr.predict(test_data)
-expected = test_target
-
-df_output = pd.DataFrame()
-
-df_output["expected"] = pd.Series(expected)
-df_output["predicted"] = pd.Series(predicted)
+df_output["class_type"] = types
 
 print(df_output)
 
-df_output.to_csv(r"C:\Users\Kennedy'sPC\Documents\Baylor University\Fall 2020 - Senior Year\MIS 4322\machinelearning\outcomes.csv", index=False, header=True)
+#df_output.to_csv(r"")
